68de1333bd089f9b1bcc8c7f9b717df314f04d0a
Ajaykumar Polsani <ajayraop@gmail.com>
2024-11-16 15:04:13 +0000

    add cross browser capability in driverManager

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -3,6 +3,9 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverManager {
 
@@ -10,8 +13,27 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            // Get browser type from system property (default to Chrome)
+            String browser = System.getProperty("browser", "chrome").toLowerCase();
+            switch (browser) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                case "safari":
+                    // SafariDriver requires Safari to be enabled for remote automation
+                    driver = new SafariDriver();
+                    break;
+                case "chrome":
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+            }
             driver.manage().window().maximize();
         }
         return driver;
